0324c362e58d39cf37b1bfbffb7d3ffe3474a087
Eugene Koifman <ekoifman@apache.org>
2018-09-27 13:25:43 -0700

    BUG-122564:HIVE-16812: VectorizedOrcAcidRowBatchReader doesn't filter delete events (Eugene Koifman, reviewed by Sergey Shelukhin)
    Change-Id: Id6bf28950377a8381423aab6f4460845a69cd1f5
    (cherry picked from commit 8d4b6d1f2267c99c4de960d49ebea325562df9c6)
    (cherry picked from commit 444d67659bf979b90a9e6c4ac95589ee81d1ea2c)

--- ql/src/test/org/apache/hadoop/hive/ql/io/orc/TestInputOutputFormat.java
+++ ql/src/test/org/apache/hadoop/hive/ql/io/orc/TestInputOutputFormat.java
@@ -802,7 +802,7 @@ public class TestInputOutputFormat {
       System.out.println("STATS TRACE END - " + testCaseName.getMethodName());
       int delta = readsAfter - readsBefore;
       //HIVE-16812 adds 1 read of the footer of each file
-      assertEquals(12, delta);
+      assertEquals(16, delta);
     } finally {
       MockFileSystem.clearGlobalFiles();
     }
